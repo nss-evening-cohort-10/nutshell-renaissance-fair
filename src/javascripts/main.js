0b4c7b7f1a57cbf5d220ce76cd64e7532dc4b518
Jamie <phillipsja97@gmail.com>
2019-11-15 18:38:29 -0600

    data is reading as an array

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,9 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import apiKeys from './helpers/apiKeys.json';
 import '../styles/main.scss';
+import 'bootstrap';
+import souvenirData from './helpers/data/souvenirData';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
+  souvenirData.getSouvenirData();
 };
 
 init();
