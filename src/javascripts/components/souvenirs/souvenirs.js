791eec48bac6a34a5faa550603dcda25df037693
Jamie <phillipsja97@gmail.com>
2019-11-20 13:22:36 -0600

    fixed

--- src/javascripts/components/souvenirs/souvenirs.js
+++ src/javascripts/components/souvenirs/souvenirs.js
@@ -11,12 +11,13 @@ const buildSouvenirs = () => {
     .then((souvenirs) => {
       let domString = '';
       if (userSignedIn) {
-        domString = '<h2 class="text-center">Souvenirs</h2>';
-        domString += '<div class="d-flex justify-content-center"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addSouvenirModal">';
-        domString += ' <div class="cardZone d-flex flex-wrap justify-content-center" id="souvenirCards">';
+        domString += '<h2 class="text-center">Souvenirs</h2>';
+        domString += '<div class="d-flex justify-content-center"><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addSouvenirModal">Add New Souvenir</button></div>';
+        domString += '<div class="cardZone d-flex flex-wrap justify-content-center" id="souvenirCards">';
       } else {
-        domString = '<h2 class="text-center">Souvenirs</h2>';
-      } domString += ' <div class="cardZone d-flex flex-wrap justify-content-center" id="souvenirCards">';
+        domString += '<h2 class="text-center">Souvenirs</h2>';
+        domString += ' <div class="cardZone d-flex flex-wrap justify-content-center" id="souvenirCards">';
+      }
       souvenirs.forEach((souvenir) => {
         domString += souvenirCard.souvenirDomString(souvenir);
       });
