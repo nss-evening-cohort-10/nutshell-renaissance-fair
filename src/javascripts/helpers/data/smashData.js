0b7678e6ad7c2decf8e5df2c41ca99993bdf2c95
Charity Bunyon <cseymou5@gmail.com>
2019-12-02 21:34:42 -0600

    food smash

--- src/javascripts/helpers/data/smashData.js
+++ src/javascripts/helpers/data/smashData.js
@@ -1,5 +1,7 @@
 import eventData from './eventData';
+import foodsData from './foodsData';
 import eventStaffData from './eventStaffData';
+import eventFoodData from './eventFoodData';
 import staffsData from './staffsData';
 
 const staffForEvent = (eventId) => new Promise((resolve, reject) => {
@@ -18,15 +20,35 @@ const staffForEvent = (eventId) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+const foodForEvent = (eventId) => new Promise((resolve, reject) => {
+  eventFoodData.getEventFoodByEventId(eventId)
+    .then((eventFood) => {
+      foodsData.getFoods().then((food) => {
+        const finalFood = [];
+        eventFood.forEach((eFood) => {
+          const newFood = food.find((x) => x.id === eFood.foodId);
+          newFood.eventFoodId = eFood.id;
+          finalFood.push(newFood);
+        });
+        resolve(finalFood);
+      });
+    })
+    .catch((error) => reject(error));
+});
+
+
 // Only calling stuff in this function
 const completeSingleEvent = (eventId) => new Promise((resolve, reject) => {
   eventData.getEventById(eventId)
     .then((event) => {
       staffForEvent(eventId).then((eventStaff) => {
-        const finalEvent = { ...event };
-        finalEvent.id = eventId;
-        finalEvent.staff = eventStaff;
-        resolve(finalEvent);
+        foodForEvent(eventId).then((eventFood) => {
+          const finalEvent = { ...event };
+          finalEvent.id = eventId;
+          finalEvent.staffs = eventStaff;
+          finalEvent.foods = eventFood;
+          resolve(finalEvent);
+        });
       });
     })
     .catch((error) => reject(error));
